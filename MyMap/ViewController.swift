50ac0240f4f7d36d1b45944542a805352365e0bc
村田彩 <murataaya@murataayanomacbook-pro.local>
2019-05-29 16:34:54 +0900

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by 村田彩 on 2019/05/29.
+//  Copyright © 2019年 村田彩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
